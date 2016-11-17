852742476d5271f0a5bbde118ced0efa545f4004
scottyvg <svangilder@gmail.com>
2016-11-16 23:16:11 -0700

    vulgar rework

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,39 +4,21 @@
 /*
  * Angular 2 decorators and services
  */
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {Component} from '@angular/core';
 
 import {LoginService} from './login.service';
 import {HTTP_PROVIDERS} from '@angular/http';
 /*
- * Login
+ * Matches
  * Component
  */
 @Component({
-  selector: 'matches',
+  selector: 'login',
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
   template: require('./login.component.html'),
   // Load our main `Sass` file into our `Matches` component
-  providers: [...HTTP_PROVIDERS, LoginService],
-  directives: [],
-  pipes: []
+  providers: [...HTTP_PROVIDERS, LoginService]
 })
 export class Login {
-  myForm: FormGroup;
-    error = false;
-    errorMessage = '';
 
-    constructor(private fb: FormBuilder, private authService: AuthService) {}
-
-    onSignin() {
-      this.authService.signinUser(this.myForm.value);
-    }
-
-    ngOnInit():any {
-        this.myForm = this.fb.group({
-            email: ['', Validators.required],
-            password: ['', Validators.required],
-        });
-    }
 }
